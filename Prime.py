7faeec52d508ef32995fa54a70119b37f3d4dfa8
VIKASH KUMAR <mevikash606@gmail.com>
2023-03-13 00:17:51 +0530

    change python code

--- Prime.py
+++ Prime.py
@@ -1,12 +1,9 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-num = int(input("Enter a number: "))
-if is_prime(num):
-    print(num, "is a prime number.")
-else:
-    print(num, "is not a prime number.")
+def print_primes(n):
+    for i in range(2, n+1):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            print(i)
+
+print_primes(20)
